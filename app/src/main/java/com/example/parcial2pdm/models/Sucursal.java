1686d9439cd910cf3e9a069caa6784c15bd5151b
CesarZV23005 <zv23005@ues.edu.sv>
2026-05-16 18:19:27 -0600

    create sucursal

---
+++ app/src/main/java/com/example/parcial2pdm/models/Sucursal.java
@@ -0,0 +1,34 @@
+package com.example.parcial2pdm.models;
+
+import com.google.firebase.database.Exclude;
+
+public class Sucursal {
+    private String id;
+    private String nombre;
+    private double latitud;
+    private double longitud;
+    private float distanciaMetros; //no se guarda en Firebase
+
+    public Sucursal() {} //obligatorio para Firebase
+
+    public Sucursal(String id, String nombre, double latitud, double longitud) {
+        this.id = id;
+        this.nombre = nombre;
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public double getLatitud() { return latitud; }
+    public void setLatitud(double latitud) { this.latitud = latitud; }
+    public double getLongitud() { return longitud; }
+    public void setLongitud(double longitud) { this.longitud = longitud; }
+
+    @Exclude
+    public float getDistanciaMetros() { return distanciaMetros; }
+    @Exclude
+    public void setDistanciaMetros(float distanciaMetros) { this.distanciaMetros = distanciaMetros; }
+}
